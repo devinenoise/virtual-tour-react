c18f33bae73792808518f8572b581290b2f9b31c
Kyle Devine <devinenoise@gmail.com>
2021-02-07 02:55:23 -0800

    first info slide

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Tour',
+  headline: 'Unlimited Transactions',
+  description:
+    'Get access to our exclusive app that allows you to setup unlimited virtual tours for your customers',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: '/images/svg-3.svg',
+  alt: 'app',
+  dark: false,
+  primary: true,
+  darkText: false
+};
